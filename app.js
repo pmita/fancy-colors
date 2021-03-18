4462c2b7366fc212f8a8be5aaef3155100f0b759
pmita <panos.mita2@outlook.com>
2021-03-18 21:43:41 +0000

    scaling the input sliders

--- app.js
+++ app.js
@@ -38,6 +38,18 @@ function randomColors(){
 
         //Check for contrast
         checkTextContrast(randomColor, hexText);
+
+        //Initialize Color sliders
+        const color = chroma(randomColor);
+        //Grab all input slider per div
+        const sliders = div.querySelectorAll('.sliders input');
+        console.log(sliders);
+        //We know what each index corresponds to
+        const hue = sliders[0];
+        const brightness = sliders[1];
+        const saturation = sliders[2];
+
+        colorizeSliders( color, hue, brightness, saturation);
     });
 }
 
@@ -49,7 +61,28 @@ function checkTextContrast(color, text){
     } else{
         text.style.color = "white";
     }
-    
 }
+
+function colorizeSliders( color, hue, brightness, saturation){
+    //Scale saturation
+    const noSaturation = color.set('hsl.s', 0); //We can desaturate the selected color
+    const fullSaturation = color.set('hsl.s', 1); //We can also oversature the selected color
+    //Create a spectrum of [noSaturation, fullSaturation] with color being the midpoint
+    const scaleSaturation = chroma.scale([noSaturation, color, fullSaturation]);
+    //Update the input specturm accordingly
+    saturation.style.backgroundImage = `linear-gradient(to right, ${scaleSaturation(0)}, ${scaleSaturation(1)})`;
+
+    //We do the same with brightness
+    //However brighness we only need the midpoint set
+    //Since the edge are either white or black
+    const midBright = color.set('hsl.l', 0.5);
+    const scaleBrightness = chroma.scale(['black', midBright, 'white']);
+    brightness.style.backgroundImage = `linear-gradient(to right, ${scaleBrightness(0)}, ${scaleBrightness(0.5)}, ${scaleBrightness(1)})`;
+
+    //Hue sliders are alwayrs the same so no need for chroma
+    hue.style.backgroundImage = `linear-gradient(to right, rgb(204, 75,75), rgb(204, 204, 75), rgb(75, 204, 75), rgb(75, 204, 204), rgb(75, 75, 204), rgb(204, 75, 204), rgb(204, 75, 75))`
+}
+
+
 
 randomColors();
